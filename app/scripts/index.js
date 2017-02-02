3bb8d21ea9c320a02d35ab9eae6f8f84de13c135
John Baldwin III <john.baldwin.jb3@gmail.com>
2017-02-02 16:55:54 -0500

    beginning JS

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,14 +2,35 @@ var $ = require('jquery');
 var _ = require('underscore');
 var Handlebars = require('handlebars');
 var githubtoken = require('./gitapikey.js');
-
-
+var url = "https://api.github.com/users/johnbaldwin3";
+var repoURL = "https://api.github.com/users/johnbaldwin3/repos";
 //send auth. token to github if token is provided
 
 if(githubtoken !== undefined) {
   $.ajaxSetup({
     headers: {
-      'Authorization' : 'token' + githubtoken.token
+      'Authorization' : 'token ' + githubtoken.token
     }
   });
 }
+
+// function start(ajaxResults){
+//   console.log(ajaxResults);
+//   var repos = $('#')
+//
+// }
+
+//sort data by chrono order inside function?
+//_.sort?
+
+$.ajax(url).done(function(data){
+  console.log(data);
+});
+
+$.ajax(repoURL).done(function(data){
+  console.log(data);
+  _.each(function(data){
+
+
+  })
+});
